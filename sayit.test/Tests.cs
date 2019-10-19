ff1644bd3786ad15210da1e5c3df451a1028923f
Mike OShea <oshea00@gmail.com>
2019-10-19 09:02:03 -0700

    Test fixture cleanup

--- sayit.test/Tests.cs
+++ sayit.test/Tests.cs
@@ -9,37 +9,37 @@ namespace Tests
         public void CanFindConsecutiveDigitsInEmptyString()
         {
             var str = "";
-            Assert.AreEqual(("",""),LookAndSay.NextSequence(str));
+            Assert.AreEqual(("",""), NextSequence(str));
         }
 
         [Test]
         public void CanFindConsecutiveDigitsInNonEmptyString()
         {
             var str = "11";
-            Assert.AreEqual(("21", ""), LookAndSay.NextSequence(str));
+            Assert.AreEqual(("21", ""), NextSequence(str));
             str = "2211";
-            Assert.AreEqual(("22", "11"), LookAndSay.NextSequence(str));
+            Assert.AreEqual(("22", "11"), NextSequence(str));
         }
 
         [Test]
         public void CanSayNextStringFromStartingString()
         {
             var str = "11";
-            Assert.AreEqual("21", LookAndSay.ExpandSequence(str));
+            Assert.AreEqual("21", ExpandSequence(str));
             str = "112";
-            Assert.AreEqual("2112", LookAndSay.ExpandSequence(str));
+            Assert.AreEqual("2112", ExpandSequence(str));
         }
 
         [Test]
         public void CanLookAndSay()
         {
-            Assert.AreEqual("11131221133112132113212221", LookAndSay.Say(1, 11));
+            Assert.AreEqual("11131221133112132113212221", Say(1, 11));
         }
 
         [Test]
         public void CanLookAndSayStartingWith9()
         {
-            Assert.AreEqual("132113213221133112132123222119", LookAndSay.Say(9, 11));
+            Assert.AreEqual("132113213221133112132123222119", Say(9, 11));
         }
     }
 }
